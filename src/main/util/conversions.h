9e269d9bf24456c0bd1677da777bf4ead6e8a3ec
Justin Hu <justin.hu@alumni.ubc.ca>
2020-04-16 13:58:05 -0700

    change to license under GPLv3.0+

--- src/main/util/conversions.h
+++ src/main/util/conversions.h
@@ -1,18 +1,21 @@
 // Copyright 2020 Justin Hu
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
 // This file is part of the T Language Compiler.
+//
+// The T Language Compiler is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// The T Language Compiler is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// The T Language Compiler. If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 /**
  * @file
@@ -22,8 +25,8 @@
 #ifndef TLC_UTIL_CONVERSIONS_H_
 #define TLC_UTIL_CONVERSIONS_H_
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 /**
  * converts a char to an unsigned byte
@@ -33,14 +36,14 @@ uint8_t charToU8(char c);
 
 /**
  * converts an unsigned byte to a hex nybble
- * 
+ *
  * @param n value to convert - must be in the range [0, 15]
  */
 char u8ToNybble(uint8_t n);
 
 /**
  * is the character a hex nybble? (i.e. is c in [0-9a-fA-F])
- * 
+ *
  * @param c character to query
  */
 bool isNybble(char c);
