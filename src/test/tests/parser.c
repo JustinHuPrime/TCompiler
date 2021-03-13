056f04c5f0fd7747c20884d24cb6f17321dd1d3f
Justin Hu <justin.hu@alumni.ubc.ca>
2021-03-12 22:07:49 -0800

    add union parsing tests

--- src/test/tests/parser.c
+++ src/test/tests/parser.c
@@ -404,7 +404,34 @@ static void testStructDeclParser(void) {
 }
 
 static void testUnionDeclParser(void) {
-  // TODO
+  FileListEntry entries[2];
+  fileList.entries = &entries[0];
+  fileList.size = 2;
+
+  entries[0].inputFilename = "testFiles/parser/unionOneOption.td";
+  entries[0].isCode = false;
+  entries[0].errored = false;
+  entries[1].inputFilename = "testFiles/parser/empty.tc";
+  entries[1].isCode = true;
+  entries[1].errored = false;
+  test("parser accepts the file", parse() == 0);
+  test("file has not errored", entries[0].errored == false);
+  test("ast is correct",
+       dumpEqual(&entries[0], "testFiles/parser/expected/unionOneOption.txt"));
+  nodeFree(entries[0].ast);
+  nodeFree(entries[1].ast);
+
+  entries[0].inputFilename = "testFiles/parser/unionManyOptions.td";
+  entries[0].isCode = false;
+  entries[0].errored = false;
+  entries[1].errored = false;
+  test("parser accepts the file", parse() == 0);
+  test("file has not errored", entries[0].errored == false);
+  test(
+      "ast is correct",
+      dumpEqual(&entries[0], "testFiles/parser/expected/unionManyOptions.txt"));
+  nodeFree(entries[0].ast);
+  nodeFree(entries[1].ast);
 }
 
 static void testEnumDeclParser(void) {
