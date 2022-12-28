eecc2c844d7b50be48060cb9036196cba0251f30
Justin Hu <justin.hu@alumni.ubc.ca>
2022-12-28 05:08:32 -0800

    main asm generation logic

--- src/main/arch/x86_64-linux/asm.c
+++ src/main/arch/x86_64-linux/asm.c
@@ -23,8 +23,13 @@
 #include "ir/ir.h"
 #include "translation/translation.h"
 #include "util/container/stringBuilder.h"
+#include "util/functional.h"
 #include "util/internalError.h"
 #include "util/numericSizing.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// misc stuff                                                                //
+///////////////////////////////////////////////////////////////////////////////
 
 size_t const X86_64_LINUX_REGISTER_WIDTH = 8;
 size_t const X86_64_LINUX_STACK_ALIGNMENT = 16;
@@ -39,6 +44,8 @@ static char const *const REGISTER_NAMES[] = {
 char const *x86_64LinuxPrettyPrintRegister(size_t reg) {
   return REGISTER_NAMES[reg];
 }
+
+// assembly data structures
 
 static void x86_64LinuxOperandFree(X86_64LinuxOperand *o) {
   switch (o->kind) {
@@ -126,7 +133,11 @@ void x86_64LinuxFileFree(X86_64LinuxFile *file) {
   free(file);
 }
 
-static char *x86_64LinuxDataToString(Vector *v) {
+///////////////////////////////////////////////////////////////////////////////
+// data frags                                                                //
+///////////////////////////////////////////////////////////////////////////////
+
+static char *dataToString(Vector const *v) {
   char *data = strdup("");
   for (size_t idx = 0; idx < v->size; ++idx) {
     IRDatum *d = v->elements[idx];
@@ -197,7 +208,7 @@ static char *x86_64LinuxDataToString(Vector *v) {
   return data;
 }
 
-static void x86_64LinuxGenerateDataAsm(IRFrag *frag, FileListEntry *file) {
+static void generateDataAsm(IRFrag const *frag, FileListEntry *file) {
   char *section;
   switch (frag->type) {
     case FT_BSS: {
@@ -232,7 +243,7 @@ static void x86_64LinuxGenerateDataAsm(IRFrag *frag, FileListEntry *file) {
       error(__FILE__, __LINE__, "invalid fragment name type");
     }
   }
-  char *data = x86_64LinuxDataToString(&frag->data.data.data);
+  char *data = dataToString(&frag->data.data.data);
 
   vectorInsert(
       &((X86_64LinuxFile *)&file->asmFile)->frags,
@@ -242,148 +253,9 @@ static void x86_64LinuxGenerateDataAsm(IRFrag *frag, FileListEntry *file) {
   free(data);
 }
 
-typedef struct {
-  /**
-   * what kind is allowed
-   */
-  OperandKind kind;
-  /**
-   * detail for each kind
-   */
-  union {
-    struct {
-      /**
-       * allowed alignments; empty = allow any
-       */
-      SizeVector alignments;
-      /**
-       * allowed sizes; empty = allow any
-       */
-      SizeVector sizes;
-      /**
-       * allowed alloc kinds; empty = allow any
-       */
-      SizeVector kinds;
-    } temp;
-    struct {
-      /**
-       * allowed sizes; empty = allow any
-       */
-      SizeVector sizes;
-    } reg;
-    struct {
-      /**
-       * allowed sizes; empty = allow any
-       */
-      SizeVector sizes;
-      /**
-       * allowed datum type (must be single datum); empty = allow any
-       */
-      SizeVector types;
-    } constant;
-  } data;
-} IRRequirementClause;
-
-static void irRequirementClauseFree(IRRequirementClause *clause) {
-  switch (clause->kind) {
-    case OK_TEMP: {
-      sizeVectorUninit(&clause->data.temp.alignments);
-      sizeVectorUninit(&clause->data.temp.sizes);
-      sizeVectorUninit(&clause->data.temp.kinds);
-      break;
-    }
-    case OK_REG: {
-      sizeVectorUninit(&clause->data.reg.sizes);
-      break;
-    }
-    case OK_CONSTANT: {
-      sizeVectorUninit(&clause->data.constant.sizes);
-      sizeVectorUninit(&clause->data.constant.types);
-      break;
-    }
-    default: {
-      error(__FILE__, __LINE__, "invalid operand kind");
-    }
-  }
-
-  free(clause);
-}
-
-typedef struct {
-  /**
-   * list of clauses; require at least one met; empty = allow any
-   */
-  Vector clauses;
-} IRRequirement;
-
-static void irRequirementFree(IRRequirement *requirement) {
-  vectorUninit(&requirement->clauses,
-               (void (*)(void *))irRequirementClauseFree);
-  free(requirement);
-}
-
-typedef struct {
-  char *skeleton;
-  SizeVector operands;
-} AsmTemplate;
-
-static void asmTemplateFree(AsmTemplate *template) {
-  free(template->skeleton);
-  sizeVectorUninit(&template->operands);
-  free(template);
-}
-
-typedef struct {
-  /**
-   * which op this implements
-   */
-  IROperator op;
-  /**
-   * requirements for IR Operands
-   *
-   * vector of IRRequirement; one element per operand
-   */
-  Vector requirements;
-  /**
-   * generated instruction templates
-   */
-  Vector templates;
-} AsmInstruction;
-
-static void asmInstructionFree(AsmInstruction *instruction) {
-  vectorUninit(&instruction->requirements, (void (*)(void *))irRequirementFree);
-  vectorUninit(&instruction->templates, (void (*)(void *))asmTemplateFree);
-  free(instruction);
-}
-
-typedef struct {
-  /**
-   * source IROperand type
-   */
-  IRRequirement source;
-  /**
-   * destination IROperand type
-   */
-  IRRequirement destination;
-  /**
-   * generated instruction templates
-   *
-   * assumes source operand is index 1 and destination operand is index 2
-   */
-  Vector templates;
-} AsmCast;
-
-static void asmCastFree(AsmCast *cast) {
-  irRequirementFree(&cast->source);
-  irRequirementFree(&cast->destination);
-  vectorUninit(&cast->templates, (void (*)(void *))asmTemplateFree);
-  free(cast);
-}
-
-static void generateAsmInstructions(Vector *instructions, Vector *casts) {
-  vectorInit(instructions);
-  vectorInit(casts);
-}
+///////////////////////////////////////////////////////////////////////////////
+// text frags                                                                //
+///////////////////////////////////////////////////////////////////////////////
 
 static X86_64LinuxOperand *operandCreate(IROperand const *ir) {
   X86_64LinuxOperand *operand = malloc(sizeof(X86_64LinuxOperand));
@@ -469,7 +341,246 @@ static X86_64LinuxOperand *operandCreate(IROperand const *ir) {
   return operand;
 }
 
-static void x86_64LinuxGenerateTextAsm(IRFrag *frag, FileListEntry *file) {
+typedef struct {
+  /**
+   * what kind is allowed
+   */
+  OperandKind kind;
+  /**
+   * detail for each kind
+   */
+  union {
+    struct {
+      /**
+       * allowed alignments; empty = allow any
+       */
+      SizeVector alignments;
+      /**
+       * allowed sizes; empty = allow any
+       */
+      SizeVector sizes;
+      /**
+       * allowed alloc kinds; empty = allow any
+       */
+      SizeVector kinds;
+    } temp;
+    struct {
+      /**
+       * allowed sizes; empty = allow any
+       */
+      SizeVector sizes;
+    } reg;
+    struct {
+      /**
+       * allowed sizes; empty = allow any
+       */
+      SizeVector sizes;
+      /**
+       * allowed datum type (must be single datum); empty = allow any
+       */
+      SizeVector types;
+    } constant;
+  } data;
+} IRRequirementClause;
+
+typedef struct {
+  /**
+   * mode for operand (used for casting purposes)
+   */
+  X86_64LinuxOperandMode mode;
+  /**
+   * list of clauses; require at least one met; empty = allow any
+   */
+  Vector clauses;
+} IRRequirement;
+
+/**
+ * is clause satisfied by operand?
+ */
+static bool operandSatisfiesClause(IRRequirementClause const *clause,
+                                   IROperand const *operand) {
+  return false;  // TODO
+}
+/**
+ * is clause satisfied by anything that satisfies category?
+ */
+static bool clauseSatisfiesClause(IRRequirementClause const *clause,
+                                  IRRequirementClause const *category) {
+  return false;  // TODO
+}
+/**
+ * is clause satisfied by anything that satisfies requirement?
+ */
+static bool requirementSatisfiesClause(IRRequirementClause const *clause,
+                                       IRRequirement const *requirement) {
+  if (requirement->clauses.size == 0) {
+    return false;  // no - anything is allowed, but I require at least a
+                   // specific kind
+  }
+
+  for (size_t idx = 0; idx < requirement->clauses.size; ++idx) {
+    IRRequirementClause *categoryClause = requirement->clauses.elements[idx];
+    if (!clauseSatisfiesClause(clause, categoryClause)) {
+      return false;  // not satisfied - this category allows things that don't
+                     // satisfy me
+    }
+  }
+
+  // everything allowed by the requirement satisfies me
+  return true;
+}
+static void irRequirementClauseFree(IRRequirementClause *clause) {
+  switch (clause->kind) {
+    case OK_TEMP: {
+      sizeVectorUninit(&clause->data.temp.alignments);
+      sizeVectorUninit(&clause->data.temp.sizes);
+      sizeVectorUninit(&clause->data.temp.kinds);
+      break;
+    }
+    case OK_REG: {
+      sizeVectorUninit(&clause->data.reg.sizes);
+      break;
+    }
+    case OK_CONSTANT: {
+      sizeVectorUninit(&clause->data.constant.sizes);
+      sizeVectorUninit(&clause->data.constant.types);
+      break;
+    }
+    default: {
+      error(__FILE__, __LINE__, "invalid operand kind");
+    }
+  }
+
+  free(clause);
+}
+
+static bool operandSatisfiesRequirement(IRRequirement const *requirement,
+                                        IROperand const *operand) {
+  if (requirement->clauses.size == 0) {
+    return true;  // requirement allows any
+  }
+
+  for (size_t idx = 0; idx < requirement->clauses.size; ++idx) {
+    IRRequirementClause *clause = requirement->clauses.elements[idx];
+    if (operandSatisfiesClause(clause, operand)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+static bool requirementSatisfiesRequirement(IRRequirement const *requirement,
+                                            IRRequirement const *category) {
+  if (requirement->clauses.size == 0) {
+    return true;  // requirement allows any
+  }
+
+  for (size_t idx = 0; idx < requirement->clauses.size; ++idx) {
+    IRRequirementClause *clause = requirement->clauses.elements[idx];
+    if (requirementSatisfiesClause(clause, category)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+static void irRequirementFree(IRRequirement *requirement) {
+  vectorUninit(&requirement->clauses,
+               (void (*)(void *))irRequirementClauseFree);
+  free(requirement);
+}
+
+typedef struct {
+  char *skeleton;
+  SizeVector operands;
+} AsmTemplate;
+
+static void asmTemplateFree(AsmTemplate *template) {
+  free(template->skeleton);
+  sizeVectorUninit(&template->operands);
+  free(template);
+}
+
+typedef struct {
+  /**
+   * which op this implements
+   */
+  IROperator op;
+  /**
+   * requirements for IR Operands
+   *
+   * vector of IRRequirement; one element per operand
+   */
+  Vector requirements;
+  /**
+   * generated instruction templates
+   */
+  Vector templates;
+} AsmInstruction;
+
+static void instantiateInstruction(AsmInstruction const *instruction,
+                                   X86_64LinuxOperand **operands,
+                                   X86_64LinuxFrag *assembly) {
+  // TODO
+}
+static void asmInstructionFree(AsmInstruction *instruction) {
+  vectorUninit(&instruction->requirements, (void (*)(void *))irRequirementFree);
+  vectorUninit(&instruction->templates, (void (*)(void *))asmTemplateFree);
+  free(instruction);
+}
+
+typedef struct {
+  /**
+   * source IROperand type
+   */
+  IRRequirement *source;
+  /**
+   * destination IROperand type
+   */
+  IRRequirement *destination;
+  /**
+   * generated instruction templates
+   *
+   * assumes source operand is index 1 and destination operand is index 2
+   */
+  Vector templates;
+} AsmCast;
+
+static X86_64LinuxOperand *instantiateCast(AsmCast const *cast,
+                                           IROperand const *source,
+                                           X86_64LinuxFrag *assembly) {
+  return NULL;  // TODO
+}
+
+static AsmCast const *findCast(Vector const *casts,
+                               IRRequirement const *destination,
+                               IROperand const *source) {
+  for (size_t idx = 0; idx < casts->size; ++idx) {
+    AsmCast const *cast = casts->elements[idx];
+    if (requirementSatisfiesRequirement(destination, cast->destination) &&
+        operandSatisfiesRequirement(cast->destination, source)) {
+      return cast;
+    }
+  }
+
+  return NULL;
+}
+
+static void asmCastFree(AsmCast *cast) {
+  irRequirementFree(cast->source);
+  irRequirementFree(cast->destination);
+  vectorUninit(&cast->templates, (void (*)(void *))asmTemplateFree);
+  free(cast);
+}
+
+static void generateAsmInstructions(Vector *instructions, Vector *casts) {
+  vectorInit(instructions);
+  vectorInit(casts);
+}
+
+static void generateTextAsm(IRFrag const *frag, FileListEntry *file,
+                            Vector const *asmInstructions,
+                            Vector const *asmCasts) {
   X86_64LinuxFile *asmFile = file->asmFile;
   X86_64LinuxFrag *assembly = x86_64LinuxTextFragCreate(
       format("section .text\nglobal %s:function\n%s:\n", frag->name.global,
@@ -479,7 +590,99 @@ static void x86_64LinuxGenerateTextAsm(IRFrag *frag, FileListEntry *file) {
   for (ListNode *currInst = b->instructions.head->next;
        currInst != b->instructions.tail; currInst = currInst->next) {
     IRInstruction *ir = currInst->data;
-    // TODO
+
+    // find appropriate instruction template
+    AsmInstruction const *selectedInstruction = NULL;
+    AsmCast const **selectedCasts = NULL;
+    size_t selectedRequiredCasts = 0;
+    for (size_t instructionIdx = 0; instructionIdx < asmInstructions->size;
+         ++instructionIdx) {
+      AsmInstruction const *candidateInstruction =
+          asmInstructions->elements[instructionIdx];
+
+      // must implement the right op
+      if (candidateInstruction->op != ir->op) {
+        continue;
+      }
+
+      AsmCast const **candidateCasts =
+          malloc(sizeof(AsmCast *) * candidateInstruction->requirements.size);
+      size_t candidateRequiredCasts = 0;
+
+      // for each operand, must be satisfied or able to convert it
+      bool requirementsSatisfiable = true;
+      for (size_t argIdx = 0; argIdx < candidateInstruction->requirements.size;
+           ++argIdx) {
+        IRRequirement *requirement =
+            candidateInstruction->requirements.elements[argIdx];
+        IROperand *operand = ir->args[argIdx];
+
+        if (operandSatisfiesRequirement(requirement, operand)) {
+          // if the requirement is directly satisfied, move on
+          candidateCasts[argIdx] = NULL;
+          continue;
+        }
+
+        // find required cast
+        AsmCast const *found = findCast(asmCasts, requirement, operand);
+        if (found == NULL) {
+          // can't satisfy requirements; stop
+          requirementsSatisfiable = false;
+          break;
+        }
+
+        // save required cast
+        candidateCasts[argIdx] = found;
+        ++candidateRequiredCasts;
+      }
+
+      if (!requirementsSatisfiable) {
+        // this isn't a candidate; move on
+        free(candidateCasts);
+        continue;
+      }
+
+      // compare against current selected instruction
+      if (selectedInstruction == NULL) {
+        // first candidate; select it
+        selectedInstruction = candidateInstruction;
+        selectedCasts = candidateCasts;
+        selectedRequiredCasts = candidateRequiredCasts;
+      } else if (selectedRequiredCasts > candidateRequiredCasts) {
+        // better than current candidate; select it
+        selectedInstruction = candidateInstruction;
+        free(selectedCasts);
+        selectedCasts = candidateCasts;
+        selectedRequiredCasts = candidateRequiredCasts;
+      } else {
+        // this wasn't better; clean up and move on
+        free(candidateCasts);
+        continue;
+      }
+    }
+
+    if (selectedInstruction == NULL) {
+      error(__FILE__, __LINE__, "no instruction template found");
+    }
+
+    // get operands (with casts)
+    X86_64LinuxOperand **operands =
+        malloc(sizeof(IROperand *) * selectedInstruction->requirements.size);
+    for (size_t argIdx = 0; argIdx < selectedInstruction->requirements.size;
+         ++argIdx) {
+      if (selectedCasts[argIdx] == NULL) {
+        // no cast required
+        operands[argIdx] = operandCreate(ir->args[argIdx]);
+      } else {
+        // do cast
+        operands[argIdx] =
+            instantiateCast(selectedCasts[argIdx], ir->args[argIdx], assembly);
+      }
+    }
+    free(selectedCasts);  // done with casts
+
+    // generate instruction
+    instantiateInstruction(selectedInstruction, operands, assembly);
   }
   vectorInsert(&asmFile->frags, assembly);
 }
@@ -499,11 +702,11 @@ void x86_64LinuxGenerateAsm(void) {
         case FT_BSS:
         case FT_RODATA:
         case FT_DATA: {
-          x86_64LinuxGenerateDataAsm(frag, file);
+          generateDataAsm(frag, file);
           break;
         }
         case FT_TEXT: {
-          x86_64LinuxGenerateTextAsm(frag, file);
+          generateTextAsm(frag, file, &asmInstructions, &asmCasts);
           break;
         }
         default: {
